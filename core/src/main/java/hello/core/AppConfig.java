123252e675438ce905214f4c54cc3fdd3dea9147
Lee <https://github.com/hehepduk/SpringStart.git>
2021-02-25 15:34:13 +0900

    https://github.com/hehepduk/SpringCore.git

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,50 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscontPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceIml;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberReposiotry()
+    //@Bean orderSerivce -> new MemoryMemberRepository()
+
+    @Bean
+    public MemberService memberService(){
+
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
+
+        return new OrderServiceIml(memberRepository(), discountPolicy());
+    }
+
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+
+        return new RateDiscontPolicy();
+    }
+
+
+}
